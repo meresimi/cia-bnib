e014a08da3bd1eeefa1f38b17eb5ed3ec2d334eb
Simiona Bobai <70103690+meresimi@users.noreply.github.com>
2026-06-07 18:49:25 +1100

    Add files via upload

--- android/app/src/main/java/com/cia/app/MainActivity.java
+++ android/app/src/main/java/com/cia/app/MainActivity.java
@@ -2,10 +2,14 @@ package com.cia.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.capacitorjs.plugins.filesystem.FilesystemPlugin;
+import com.capacitorjs.plugins.share.SharePlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(FilesystemPlugin.class);
+        registerPlugin(SharePlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
