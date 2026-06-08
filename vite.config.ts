a2bcb19dc869562038e4a18b963f574f6e0c1da4
Simiona Bobai <70103690+meresimi@users.noreply.github.com>
2026-06-08 17:29:53 +1100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          exceljs: ['exceljs']
+          jszip: ['jszip']
         }
       }
     }
